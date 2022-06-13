3e4565d95ea045ac9ecbb940d58ce031ae05449b
Slava Melanko <slava.melanko@gmail.com>
2022-06-13 20:39:28 +0300

    Added few more tests

--- blog/cpp20/coroutines/code/tests/Task.cpp
+++ blog/cpp20/coroutines/code/tests/Task.cpp
@@ -2,12 +2,25 @@
 
 #include <Task.h>
 
-coro::Task<int> Square(int val) {
-  co_return(val * val);
+coro::Task<int> Square(int val) { co_return(val * val); }
+
+TEST(TaskTest, InitialState) {
+  coro::Task<void> taskVoid;
+  EXPECT_FALSE(taskVoid);
+  coro::Task<int> taskInt;
+  EXPECT_FALSE(taskInt);
 }
 
-TEST(TaskTest, Square)
-{
+TEST(TaskTest, ReturnVoid) {
+  auto task = []() -> coro::Task<void> { co_return; }();
+  EXPECT_TRUE(task);
+  if (task) {
+    task.resume();
+  }
+  EXPECT_FALSE(task);
+}
+
+TEST(TaskTest, ReturnInt) {
   auto task = Square(2);
   EXPECT_TRUE(task);
   if (task) {
